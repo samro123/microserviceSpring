7db4a7adceff00355c281a94016e36f1f23c228b
samro123 <thsam.20it5@vku.udn.vn>
2024-09-25 08:19:07 +0700

    Authentication va authorization giua ca micro-service

--- identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
+++ identity-service/src/main/java/com/devteria/identity/repository/httpclient/ProfileClient.java
@@ -1,8 +1,8 @@
 package com.devteria.identity.repository.httpclient;
 
+import com.devteria.identity.configuration.AuthenticationRequestInterceptor;
 import com.devteria.identity.dto.request.ProfileCreationRequest;
 import com.devteria.identity.dto.response.UserProfileResponse;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 
-@FeignClient(name = "profile-service", url = "${app.services.profile}")
+@FeignClient(name = "profile-service", url = "${app.services.profile}",
+        configuration = {AuthenticationRequestInterceptor.class})
 public interface ProfileClient {
     @PostMapping(value = "/internal/users", produces = MediaType.APPLICATION_JSON_VALUE)
     UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request);
